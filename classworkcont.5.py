e4655833c58379adc31547e07a0ab396067e1c1b
liana <hilllia@ssfs.org>
2019-09-26 14:15:48 -0400

    worked on classroom exercise problem 5

---
+++ classworkcont.5.py
@@ -0,0 +1,11 @@
+# by Liana Hill
+# September 26, 2019
+# daily exercises number 5
+
+import math
+def triangle_sides(a, b, c):
+    s = (a + b + c)/2
+    A = math.sqrt(s * (s-a))
+
+def main():
+    answer =
