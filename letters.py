2ac619e8a830085a1d4b51fd09b11411d8426d6c
liana <hilllia@ssfs.org>
2019-10-03 10:44:15 -0400

    worked on 'mississippi' lettering

--- letters.py
+++ letters.py
@@ -14,6 +14,7 @@ def goto(x, y):
     turtle.goto(x, y)
     turtle.pendown()
 
+
 # M function
 def make_m():
     turtle.seth(90)
@@ -25,6 +26,7 @@ def make_m():
     turtle.rt(140)
     turtle.fd(90)
 
+# makes letter i
 def make_i():
     turtle.lt(90)
     turtle.fd(20)
@@ -37,17 +39,50 @@ def make_i():
     turtle.rt(180)
     turtle.fd(20)
 
+
 def make_s():
     turtle.rt(90)
     for x in range(5):
         turtle.fd(20)
         turtle.lt(45)
-    turtle.fd(40)
+    turtle.fd(60)
+    turtle.seth(90)
+    turtle.fd(5)
+    turtle.rt(45)
+    turtle.fd(20)
+    turtle.seth(0)
+    turtle.fd(20)
+    turtle.rt(45)
+    turtle.fd(20)
+    turtle.rt(45)
+
+def make_p():
+    pass
 
 make_m()
+
 goto(-375, 0)
 make_i()
+
 goto(-345, 30)
 make_s()
+
+goto(-275, 30)
+turtle.lt(90)
+make_s()
+
+goto(-245, 0)
+make_i()
+goto(-190, 30)
+
+make_s()
+goto(-150, 30)
+turtle.lt(90)
+
+make_s()
+goto(-145, 0)
+
+make_i()
+goto(-90, 0)
 
 turtle.exitonclick()
